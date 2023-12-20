f7f94903b996eb07f0558703271ee5d3c1776773
monakogpt <124113151+monakogpt@users.noreply.github.com>
2023-12-20 17:22:32 -0500

    Add files via upload

---
+++ WebApiAutores/WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Controllers.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get() 
+        {
+            return new List<Autor>() { 
+                new Autor() {Id =1, Nombre="Felipe" },
+                new Autor() {Id =2, Nombre="Claudia"}
+
+            };
+         
+        }
+
+    }
+}
